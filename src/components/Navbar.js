495086735906a36e97ced4bc90e62111c8a25028
Magaramova <jasmin_oir@mail.ru>
2023-11-20 16:44:57 +0300

    add new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
+
+
 export default  function Navbar(){
     return <nav  className = "nav">
-        <a href="/"  className="site-title " >Канбан</a>
 
         <ul>
             <li> <a href="/create_task"></a>Создать задачу </li>
