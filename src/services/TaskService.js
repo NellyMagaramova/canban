2905d43b6cccfa15556c9bbcaa9d83e32d1bf6a1
Magaramova <jasmin_oir@mail.ru>
2023-12-01 10:23:27 +0300

    add  taskservice

---
+++ src/services/TaskService.js
@@ -0,0 +1,45 @@
+import http from "../http-common";
+
+
+import http from "../http-common";
+
+const getAll = () => {
+    return http.get("/task");
+};
+
+const get = id => {
+    return http.get(`/task/${id}`);
+};
+
+const create = data => {
+    return http.post("/task", data);
+};
+
+const update = (id, data) => {
+    return http.put(`/task/${id}`, data);
+};
+
+const remove = id => {
+    return http.delete(`/task/${id}`);
+};
+
+const removeAll = () => {
+    return http.delete(`/task`);
+};
+
+const findByTitle = title => {
+    return http.get(`/tutorials?title=${title}`);
+};
+
+const TaskService =
+    {
+        getAll,
+        get,
+        create,
+        update,
+        remove,
+        removeAll,
+        findByTitle
+    };
+
+export default TaskService;
